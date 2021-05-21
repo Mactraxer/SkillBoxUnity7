041429ce253dfa9b3cbdcbedd89b6e49f0f463c1
Mactraxer <jackvo_26rus@mail.ru>
2021-05-21 18:35:07 +0300

    Initial commit

---
+++ SkillBox7/Assets/Scripts/GameController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TickFrom
+{
+    warriorTrain, warriorEat, peasantTrain, peasantEat, wheatGet, battle
+}
+public interface ITickable
+{
+    void Tick(TickFrom from);
+}
+public class GameController : MonoBehaviour, ITickable
+{
+
+    public int warriorCount;
+    public int peasantCount;
+    public int wheatCount;
+
+    public int warriorEatCount;
+    public int peasantEatCount;
+    
+    public void Tick(TickFrom from)
+    {
+        switch(from)
+        {
+            case TickFrom.warriorTrain:
+                TrainWarrior();
+                break;
+            case TickFrom.warriorEat:
+                WarriorEat();
+                break;
+            case TickFrom.peasantTrain:
+                PeasantEat();
+                break;
+            default:
+                break;
+        }
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        warriorCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void TrainWarrior()
+    {
+
+    }
+
+    private void TrainPeasant()
+    {
+
+    }
+
+    private void WarriorEat()
+    {
+
+    }
+
+    private void PeasantEat()
+    {
+
+    }
+
+    private void StartBattle()
+    {
+
+    }
+
+    private void WheatGet()
+    {
+
+    }
+}
---
+++ SkillBox7/Assets/Scripts/ProgressTime.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ProgressTime : MonoBehaviour
+{
+
+    [SerializeField] Image changableImage;
+    [SerializeField] float maxTime;
+
+    public ITickable eventSender;
+
+    private float currentTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = maxTime;
+        eventSender = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= Time.deltaTime;
+
+        if (currentTime <= 0)
+        {
+            currentTime = maxTime;
+            eventSender.Tick(TickFrom.warrior);
+        }
+
+        changableImage.fillAmount = currentTime / maxTime;
+    }
+
+}
